b8762a06fd10af06f0e88c0b15f4fb67fa801f45
Hannan <hannanfaryad@gmail.com>
2021-08-18 16:43:12 +0500

    imported styles in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from "lodash";
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
 
   //loadash now imported from this scriptnp
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
